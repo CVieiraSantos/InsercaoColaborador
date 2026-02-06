3ca9ee3bc4742a8999eb12be0d483338e517e9d1
Carlos Vieira <cadu.vieira2276@gmail.com>
2026-02-06 14:58:37 -0300

    Add project files.

---
+++ InsercaoColaborador/Entities/ColaboradorExcel.cs
@@ -0,0 +1,9 @@
+﻿namespace InsercaoColaborador.Entities
+{
+    public class ColaboradorExcel
+    {
+        public string CNS { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+    }
+}
